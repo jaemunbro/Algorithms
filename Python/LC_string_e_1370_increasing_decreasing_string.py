2fdbfe59a5b362bae1a95fb5677cc9450dc9d68d
ryan.jung <ryan.jung@balancehero.com>
2020-09-02 23:59:13 +0900

    Add different solution for inc_dec_string

--- Python/LC_string_e_1370_increasing_decreasing_string.py
+++ Python/LC_string_e_1370_increasing_decreasing_string.py
@@ -3,6 +3,8 @@ One way to solve, but regarding time complexity, s * s * sorting = O(logN**3)
 
 Let's consider a better time complexity solution with collections.Counter.
 '''
+from collections import Counter
+
 class Solution:
     def sortString(self, s: str) -> str:
         s = list(s)
@@ -15,3 +17,28 @@ class Solution:
                 s.remove(letter)
                 result += letter
             return result
+
+    def sortString_with_Counter(self, s: str) -> str:
+        s_dict = Counter(s)
+        result = ''
+
+        while s_dict:
+            for char in sorted(s_dict):
+                temp = s_dict[char]
+                result += char
+
+                if temp == 1:
+                    s_dict.pop(char)
+                else:
+                    s_dict[char] = temp - 1
+
+            for char in sorted(s_dict, reverse = True):
+                temp = s_dict[char]
+                result += char
+
+                if temp == 1:
+                    s_dict.pop(char)
+                else:
+                    s_dict[char] = temp - 1
+
+        return result
