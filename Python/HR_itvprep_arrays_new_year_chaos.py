b8d729c2a6b18f4c9f347e12fac8eb3b1e077ab3
ryan.jung <ryan.jung@balancehero.com>
2021-01-26 01:29:44 +0900

    Add new year chaos

--- Python/HR_itvprep_arrays_new_year_chaos.py
+++ Python/HR_itvprep_arrays_new_year_chaos.py
@@ -1,6 +1,30 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
 t = int(input())
 for i in range(t):
     num_ppl = int(input())
     n = list(map(int, input().split()))
 
+    # org_line = list(range(1,num_ppl+1))
+    # for i in range(1,num_ppl+1):
+    #     org_line.append(i)
+
+    sum = 0
+    for idx, num in enumerate(n):
+        # result = org_line.index(num) - idx
+
+        if num - (idx + 1) >= 3:
+            sum = 'Too chaotic'
+            break
+
+        for j in n[max(num-2, 0):idx]:
+            if j > num:
+                sum += 1
+
+
+
+
+
+
+
+    print(sum)
---
+++ Python/HR_itvprep_arrays_new_year_chaos_02.py
@@ -0,0 +1,28 @@
+#TODO
+# with bubble sort
+def NewYearChaos(queue):
+    lastIndex = len(queue) - 1
+    swaps = 0
+    swaped = False
+
+    # check if the queue is too chaotic
+    for i, v in enumerate(queue):
+        if (v - 1) - i > 2:
+            return "Too chaotic"
+
+    # bubble sorting to find the answer
+    for i in range(0, lastIndex):
+        for j in range(0, lastIndex):
+            comps += 1
+            if queue[j] > queue[j+1]:
+                temp = queue[j]
+                queue[j] = queue[j+1]
+                queue[j+1] = temp
+                swaps += 1
+                swaped = True
+
+        if swaped:
+            swaped = False
+        else:
+            break
+    return swaps
